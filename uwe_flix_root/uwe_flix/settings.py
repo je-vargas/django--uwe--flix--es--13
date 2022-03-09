b19c5a7e5d0b23f14cd3bf706e1a0e008a6f328c
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-09 10:54:38 +0000

    setings tweak

--- uwe_flix_root/uwe_flix/settings.py
+++ uwe_flix_root/uwe_flix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5m=-e8exljd6wtm&owl0gi^wa$o&&nmrb-idtz^p9v2@0$di3w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
