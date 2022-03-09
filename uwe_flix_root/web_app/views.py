76e6fc953216abc915c6c7d02744b9ee24c042b4
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-09 10:55:07 +0000

    some view added

--- uwe_flix_root/web_app/views.py
+++ uwe_flix_root/web_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Home response is currently working</h1>")
+
+
+def club_rep_login(request):
+    return HttpResponse("the club rep url works")
