51ef6def08947126416eacd0a50e29efc25290c5
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-09 10:55:01 +0000

    urls working -> very basic

--- uwe_flix_root/uwe_flix/urls.py
+++ uwe_flix_root/uwe_flix/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', include("web_app.urls")),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')) #: using this line to stop favicon error
 ]
+
+urlpatterns += staticfiles_urlpatterns()
