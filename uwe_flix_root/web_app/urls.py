a9d2ff004d8a9acc6bfec0338780cbc5b3cf4a36
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-10 16:42:18 +0000

    added logic to display infomration from films table and get detailed views for each individual film based on film id

--- uwe_flix_root/web_app/urls.py
+++ uwe_flix_root/web_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, FilmDetailView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about")
+    path("films/<int:pk>/", FilmDetailView.as_view(), name="film_detail"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home")
 ]
--- uwe_flix_root/web_app/views.py
+++ uwe_flix_root/web_app/views.py
@@ -1,12 +1,17 @@
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
-from . import models
+from django.views.generic import ListView, DetailView
+from .models import Film
 
 template_root = "web_app"
 
 class HomePageView(ListView):
-    model = models.Film
+    model = Film
     template_name = f'{template_root}/home.html'
     context_object_name = "all_films"
+
+class FilmDetailView(DetailView):
+    model = Film
+    template_name = f'{template_root}/film_detail.html'
+
 class AboutPageView(TemplateView):
     template_name = f'{template_root}/about.html'
