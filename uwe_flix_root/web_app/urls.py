48bb39d61f0131c81c83ffa2f00aaebd8dcf282d
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-11 22:19:15 +0000

    ESD-21: New URL added to add films

--- uwe_flix_root/web_app/urls.py
+++ uwe_flix_root/web_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, FilmDetailView
+from .views import HomePageView, AboutPageView, FilmDetailView, FilmNew
 
 urlpatterns = [
     path("films/<int:pk>/", FilmDetailView.as_view(), name="film_detail"),
+    path("films/new/", FilmNew.as_view(), name="film_new"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home")
 ]
