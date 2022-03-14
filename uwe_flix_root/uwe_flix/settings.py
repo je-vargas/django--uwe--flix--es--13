e0e6a24fee660fd05ee85359ac5c3a2d56dcf42a
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-14 18:08:14 +0000

    ESD-20: reanmed accounts to be users

--- uwe_flix_root/uwe_flix/settings.py
+++ uwe_flix_root/uwe_flix/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 INSTALLED_APPS = [
     'web_app.apps.WebAppConfig',
-    'accounts.apps.AccountsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
