9fc3966b704d5351d66306ee6d927f8877b1d77b
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-16 16:52:21 +0000

    ESD-20: password change done

--- project/users/forms.py
+++ project/users/forms.py
@@ -4,12 +4,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    # username =forms.CharField(max_length=200, required=True)
     email = forms.EmailField()
     age = forms.IntegerField()
     
     class Meta:
         model = CustomUser
-        fields = ('email', 'age',  )
+        fields = ('email', 'age',  'username')
 
 class CustomUserChangeForm(UserChangeForm):
     
