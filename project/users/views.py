3693feae605529fe202e29210ab1d9b798b54f5b
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-18 16:31:27 +0000

    ESD-20: views for users

--- project/users/views.py
+++ project/users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
@@ -9,3 +11,30 @@ class SignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = "signup.html"
 
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error login in, try again")
+            return redirect('login')
+    else: 
+        return render(request, 'registration/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {})
+
+
+
+   
