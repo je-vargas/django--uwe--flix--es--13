5ca24d739a09f753a0fab2dc1a46f6349db7189f
juanE <55924277+je-vargas@users.noreply.github.com>
2022-03-29 17:34:37 +0100

    DSP-22: registration separate view added

--- project/users/views.py
+++ project/users/views.py
@@ -32,8 +32,22 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration Sucessfull!"))
+            return redirect('home')
+    else: 
         form = UserCreationForm()
-    return render(request, "registration/signup.html", {})
+    return render(request, "registration/signup.html", {
+        'form':form,
+    })
 
 
 
