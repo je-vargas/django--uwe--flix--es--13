c0da7ae28b522d2f25f29665f14d191c86f9f88c
juanE <55924277+je-vargas@users.noreply.github.com>
2022-04-02 18:00:09 +0100

    ESD-30: adding urls for showing

--- project/films/urls.py
+++ project/films/urls.py
@@ -2,16 +2,29 @@ from django.urls import path
 from .views import (
     HomePageView,
     AboutPageView,
-    FilmDetailView,
-    FilmNewView,
-    FilmUpdateView,
-    FilmDeleteView
+    FilmsDetailView,
+    FilmsNewView,
+    FilmsUpdateView,
+    FilmsDeleteView,
+    ShowingsNewView,
+    ShowingsAllView, 
+    ShowingDetailView
 ) 
 
 urlpatterns = [
-    path("films/<int:pk>/delete/", FilmDeleteView.as_view(), name="film-delete"),
-    path("films/<int:pk>/update/", FilmUpdateView.as_view(), name="film-update"),
-    path("films/<int:pk>/", FilmDetailView.as_view(), name="film-detail"),
-    path("films/new/", FilmNewView.as_view(), name="film-new"),
-    path("", HomePageView.as_view(), name="home")
+    path("", HomePageView.as_view(), name="home"),
+
+    path("films/<int:pk>/delete/", FilmsDeleteView.as_view(), name="film-delete"),
+    path("films/<int:pk>/update/", FilmsUpdateView.as_view(), name="film-update"),
+    path("films/<int:pk>/", FilmsDetailView.as_view(), name="film-detail"),
+    path("films/new/", FilmsNewView.as_view(), name="film-new"),
+
+    path("showings/<int:pk>/", ShowingDetailView.as_view(), name="showing-details"),
+    path("showings/new", ShowingsNewView.as_view(), name="showing-new"),
+    path("showings", ShowingsAllView.as_view(), name="showing-all"),
+    
+    # path("showing/<int:pk>/delete/", .as_view(), name="film-delete"),
+    # path("showing/<int:pk>/update/", .as_view(), name="film-update"),
+    # path("showing/<int:pk>/", .as_view(), name="film-detail"),
+    
 ]
