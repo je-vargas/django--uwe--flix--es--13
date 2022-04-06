5d82fc5c6ffe635421c777b1e89ecb8f86d028f3
juanE <55924277+je-vargas@users.noreply.github.com>
2022-04-06 19:57:36 +0100

    ESD-30: tweak to urls

--- project/films/urls.py
+++ project/films/urls.py
@@ -1,30 +1,21 @@
 from django.urls import path
-from .views import (
-    HomePageView,
-    AboutPageView,
-    FilmsDetailView,
-    FilmsNewView,
-    FilmsUpdateView,
-    FilmsDeleteView,
-    ShowingsNewView,
-    ShowingsAllView, 
-    ShowingDetailView
-) 
+
+from . import views
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("films/<int:pk>/delete/", views.FilmsDeleteView.as_view(), name="film-delete"),
+    path("films/<int:pk>/update/", views.FilmsUpdateView.as_view(), name="film-update"),
+    path("films/<int:pk>/", views.FilmsDetailView.as_view(), name="film-detail"),
+    path("films/new/", views.FilmsNewView.as_view(), name="film-new"),
 
-    path("films/<int:pk>/delete/", FilmsDeleteView.as_view(), name="film-delete"),
-    path("films/<int:pk>/update/", FilmsUpdateView.as_view(), name="film-update"),
-    path("films/<int:pk>/", FilmsDetailView.as_view(), name="film-detail"),
-    path("films/new/", FilmsNewView.as_view(), name="film-new"),
-
-    path("showings/<int:pk>/", ShowingDetailView.as_view(), name="showing-details"),
-    path("showings/new", ShowingsNewView.as_view(), name="showing-new"),
-    path("showings/", ShowingsAllView.as_view(), name="showing-all"),
+    path("showings/<int:pk>/", views.ShowingDetailView.as_view(), name="showing-details"),
+    path("showings/new", views.showingsNewView, name="showing-new"),
+    # path("showings/new", views.ShowingsNewView.as_view(), name="showing-new"),
+    path("showings/", views.ShowingsAllView.as_view(), name="showing-all"),
     
-    # path("showing/<int:pk>/delete/", .as_view(), name="film-delete"),
-    # path("showing/<int:pk>/update/", .as_view(), name="film-update"),
-    # path("showing/<int:pk>/", .as_view(), name="film-detail"),
+    # path("showing/<int:pk>/delete/", views.as_view(), name="film-delete"),
+    # path("showing/<int:pk>/update/", views.as_view(), name="film-update"),
+    # path("showing/<int:pk>/", views.as_view(), name="film-detail"),
     
 ]
