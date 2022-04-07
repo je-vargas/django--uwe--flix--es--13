a8b1282a4d30e55e506ea402308ab93eadc0165e
juanE <55924277+je-vargas@users.noreply.github.com>
2022-04-07 16:47:21 +0100

    ESD-30: new view added for showings

--- project/films/views.py
+++ project/films/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, redirect
 from .models import Film, Showing
-from .forms import NewFilmsForm, NewShowingsForm
+from . import forms
 
 class HomePageView(ListView):
     model = Film
@@ -14,35 +14,35 @@ class HomePageView(ListView):
 
 class FilmsDetailView(DetailView):
     model = Film
-    template_name = 'film_detail.html'
+    template_name = 'films/film_detail.html'
 
 class FilmsNewView(LoginRequiredMixin, CreateView):
     model = Film
-    template_name = 'film_new.html'
+    template_name = 'films/film_new.html'
     login_url = 'login', 
-    form_class = NewFilmsForm
+    form_class = forms.NewFilmsForm
 
 class FilmsUpdateView(LoginRequiredMixin, UpdateView):
     model = Film
-    template_name = 'film_update.html'
-    fields = '__all__'
+    template_name = 'films/film_update.html'
     login_url = 'login'
+    form_class = forms.UpdateFilmForm
 
 class FilmsDeleteView(LoginRequiredMixin, DeleteView):
     model = Film
-    template_name = template_name = 'film_delete.html'
+    template_name = template_name = 'films/film_delete.html'
     success_url = reverse_lazy('home')
     login_url = 'login'
 
     # def dispatch(self, request, *args, **kwargs): 
-    #     obj = self.get_object()
-    #     if obj.author != self.request.user:
-    #         raise PermissionDenied
-    #     return super().dispatch(request, *args, **kwargs)
+        #     obj = self.get_object()
+        #     if obj.author != self.request.user:
+        #         raise PermissionDenied
+        #     return super().dispatch(request, *args, **kwargs)
 
 def showingsNewView(request):
 
-    form = NewShowingsForm(request.POST or None)
+    form = forms.NewShowingsForm(request.POST or None)
     
     if request.method == "POST":
         
@@ -60,7 +60,7 @@ def showingsNewView(request):
 
             return redirect('showings-all')
         else:
-            return render(request, 'showing_new.html', {
+            return render(request, 'showings/showing_new.html', {
                 "form":form,
                 "film_id":request.POST.get('form-0-film-id'),
                 "time":request.POST.get('form-1-time'),
@@ -68,30 +68,31 @@ def showingsNewView(request):
                 })
         
     else:
-        form = NewShowingsForm()
-        return render(request, 'showing_new.html', {"form":form})
+        form = forms.NewShowingsForm()
+        return render(request, 'showings/showing_new.html', {"form":form})
 
 
 
 class ShowingsAllView(ListView):
     model = Showing
-    template_name = 'showing_all.html'
+    template_name = 'showings/showing_all.html'
     context_object_name = "all_showings"
 
 class ShowingDetailView(LoginRequiredMixin, DetailView):
     model = Showing
-    template_name = 'showing_detail.html'
+    template_name = 'showings/showing_detail.html'
 
 class ShowingUpdateView(LoginRequiredMixin, UpdateView):
     model = Showing
-    template_name = 'film_update.html'
-    fields = '__all__'
+    template_name = 'showings/showing_update.html'
+    success_url = reverse_lazy('showing-all')
     login_url = 'login'
+    form_class=forms.UpdateShowingForm
 
 class ShowingDeleteView(LoginRequiredMixin, DeleteView):
     model = Showing
-    template_name = template_name = 'film_delete.html'
-    success_url = reverse_lazy('home')
+    template_name = template_name = 'showings/showing_delete.html'
+    success_url = reverse_lazy('showing-all')
     login_url = 'login'
 
 class AboutPageView(TemplateView):
