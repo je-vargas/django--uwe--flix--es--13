4fdbf56c1f93b742cc98ce982e3171dc3c914a0b
juanE <55924277+je-vargas@users.noreply.github.com>
2022-04-07 16:47:50 +0100

    ESD-30: urls now have all showing options

--- project/films/urls.py
+++ project/films/urls.py
@@ -6,16 +6,13 @@ urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("films/<int:pk>/delete/", views.FilmsDeleteView.as_view(), name="film-delete"),
     path("films/<int:pk>/update/", views.FilmsUpdateView.as_view(), name="film-update"),
-    path("films/<int:pk>/", views.FilmsDetailView.as_view(), name="film-detail"),
+    path("films/<int:pk>/", views.FilmsDetailView.as_view(), name="film-details"),
     path("films/new/", views.FilmsNewView.as_view(), name="film-new"),
 
-    path("showings/<int:pk>/", views.ShowingDetailView.as_view(), name="showings-details"),
-    path("showings/new", views.showingsNewView, name="showings-new"),
-    # path("showings/new", views.ShowingsNewView.as_view(), name="showing-new"),
-    path("showings/", views.ShowingsAllView.as_view(), name="showings-all"),
-    
-    # path("showing/<int:pk>/delete/", views.as_view(), name="film-delete"),
-    # path("showing/<int:pk>/update/", views.as_view(), name="film-update"),
-    # path("showing/<int:pk>/", views.as_view(), name="film-detail"),
+    path("showings/", views.ShowingsAllView.as_view(), name="showing-all"),
+    path("showing/<int:pk>/delete/", views.ShowingDeleteView.as_view(), name="showing-delete"),
+    path("showing/<int:pk>/update/", views.ShowingUpdateView.as_view(), name="showing-update"),
+    path("showings/<int:pk>/", views.ShowingDetailView.as_view(), name="showing-details"),
+    path("showings/new", views.showingsNewView, name="showing-new"),
     
 ]
