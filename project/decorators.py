e2ac096876b51353149a960263021236021363ce
juanE <55924277+je-vargas@users.noreply.github.com>
2022-04-19 17:16:57 +0100

    ESD-26: view based restriction fully in place with registration set up for different accoutns

--- project/decorators.py
+++ project/decorators.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from access import *
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
@@ -14,13 +15,12 @@ def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group = None
-            if request.users.group.exists():
-                group = request.users.group.all[0].name
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
 
-            if group in allowed_roles:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+            if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
-                return HttpResponse("You are not authorised to view this page")
-            return view_func(request, *args, **kwargs)
+                return HttpResponse('You are not authorized to view this page')
         return wrapper_func
     return decorator
