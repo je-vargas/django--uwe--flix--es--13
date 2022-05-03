6049c4793ca2e101f6d45fba6ce318974f9902ca
juanE <55924277+je-vargas@users.noreply.github.com>
2022-05-03 16:11:33 +0100

    esd-32: simplified db models - may need futher changes for booking

---
+++ project/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Club(models.Model):
+    club_name = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=254)
+    mobile = models.IntegerField()
+    landline = models.CharField(max_length=20)
+    house_number: models.IntegerField(null=False)
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    postcode = models.CharField(max_length=10)
+    
+
+    
