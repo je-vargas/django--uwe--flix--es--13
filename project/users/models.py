eb19084e30dc3ac35ea3d8dcfe794bfa38531e06
juanE <55924277+je-vargas@users.noreply.github.com>
2022-05-04 06:50:05 +0100

    esd-36: new model created

--- project/users/models.py
+++ project/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Club(models.Model):
-    club_name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=100, null=False)
     email = models.EmailField(max_length=254)
     mobile = models.IntegerField()
     landline = models.CharField(max_length=20)
@@ -8,6 +8,10 @@ class Club(models.Model):
     street = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
     postcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        r_str = "{0}".format(self.name)
+        return r_str
     
 
     
